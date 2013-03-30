e8c4931fe976d1c8af7a19975f71707427296f52
Juan G. Hurtado <juan.g.hurtado@gmail.com>
2013-03-30 16:58:41 +0100

    Refactor todo-item view specs

--- test/specs/todo/views/todo-item.spec.js
+++ test/specs/todo/views/todo-item.spec.js
@@ -39,6 +39,54 @@ define([
     return e;
   }
 
+  function addDoneTodo() {
+    addTodo({
+      text : 'Sample',
+      done : true
+    });
+  }
+
+  function addNotDoneTodo() {
+    addTodo({
+      text : 'Sample'
+    });
+  }
+
+  function getFirstTodoInList() {
+    return _list.children.first().$el;
+  }
+
+  function getLabelOfFirstTodoInList() {
+    return getFirstTodoInList().find('label');
+  }
+
+  function setTextOfFirstTodo(text) {
+    _todos.at(0).set('text', text);
+  }
+
+  function clickToggleButtonOnFirstTodo() {
+    getFirstTodoInList().find('input.toggle').trigger('click');
+  }
+
+  function getTextFieldOfFirstTodoInList() {
+   return getFirstTodoInList().find('.edit');
+  }
+
+  function doubleClickLabelOfFirstTodoInList() {
+    getLabelOfFirstTodoInList().trigger('dblclick');
+  }
+
+  function clickDestroyButtonOfFirstTodoInList() {
+    getFirstTodoInList().find('.destroy').trigger('click');
+  }
+
+  function editTodo() {
+    var e = getEnterKeyPressed();
+
+    doubleClickLabelOfFirstTodoInList();
+    getTextFieldOfFirstTodoInList().val('Edited text').trigger(e);
+  }
+
   /* =BEFORE AND AFTER
   --------------------------------------------------------------------------- */
   beforeEach(function() {
@@ -54,90 +102,55 @@ define([
   describe("TO-DO item view", function() {
 
     it("Should have 'completed' class if todo is done", function() {
-      addTodo({
-        text : 'Sample',
-        done : true
-      });
+      addDoneTodo();
 
-      expect(_list.children.first().$el).toHaveClass('completed');
+      expect(getFirstTodoInList()).toHaveClass('completed');
     });
 
     it("Should have 'active' class if todo is not done", function() {
-      addTodo({
-        text : 'Sample'
-      });
+      addNotDoneTodo();
 
-      expect(_list.children.first().$el).toHaveClass('active');
+      expect(getFirstTodoInList()).toHaveClass('active');
     });
 
     it("Should reRender the view on model change", function() {
-      addTodo({
-        text : 'Sample'
-      });
+      addNotDoneTodo();
 
-      _todos.at(0).set('text', 'Updated text');
+      setTextOfFirstTodo('Updated text');
 
-      var $el = _list.children.first().$el;
-      var $label = $el.find('label');
-
-      expect($label).toHaveText('Updated text');
+      expect(getLabelOfFirstTodoInList()).toHaveText('Updated text');
     });
 
     it("Should update model 'done' attr when clicking on checkbox", function() {
-      addTodo({
-        text : 'Sample'
-      });
+      addNotDoneTodo();
 
-      var $toggle = _list.children.first().$el.find('input.toggle');
-
-      $toggle.trigger('click');
+      clickToggleButtonOnFirstTodo();
 
       expect(_todos.at(0).get('done')).toBe(true);
     });
 
     it("Should set focus on textfield and add 'editing' class on item after a double click on todo text", function() {
-      addTodo({
-        text : 'Sample'
-      });
+      addNotDoneTodo();
 
-      var $el = _list.children.first().$el;
-      var $textfield = $el.find('.edit');
-      var $label = $el.find('label');
+      doubleClickLabelOfFirstTodoInList();
 
-      $label.trigger('dblclick');
-
-      expect($el).toHaveClass('editing');
-      expect($textfield).toBe(':focus');
+      expect(getFirstTodoInList()).toHaveClass('editing');
+      expect(getTextFieldOfFirstTodoInList()).toBe(':focus');
     });
 
     it("Should empty the textfield and remove 'editing' class on item after updating a todo", function() {
-      addTodo({
-        text : 'Sample'
-      });
+      addNotDoneTodo();
 
-      var enterKeyPressed = getEnterKeyPressed();
+      editTodo();
 
-      var $el = _list.children.first().$el;
-      var $label = $el.find('label');
-      var $textfield = $el.find('.edit');
-
-      $label.trigger('dblclick');
-
-      $textfield.val('Edited text');
-      $textfield.trigger(enterKeyPressed);
-
-      expect($el.find('.edit').val()).toBe('');
-      expect($el).not.toHaveClass('editing');
+      expect(getTextFieldOfFirstTodoInList()).toBeEmpty();
+      expect(getFirstTodoInList()).not.toHaveClass('editing');
     });
 
     it("Should destroy the model after clicking on destroy button", function() {
-      addTodo({
-        text : 'Sample'
-      });
+      addNotDoneTodo();
 
-      var $destroy = _list.children.first().$el.find('.destroy');
-
-      $destroy.trigger('click');
+      clickDestroyButtonOfFirstTodoInList();
 
       expect(_todos.length).toBe(0);
     });
