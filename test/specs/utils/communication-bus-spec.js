ada88d6e6928b4392ca78b17a944925a58690e1e
Juan G. Hurtado <juan.g.hurtado@gmail.com>
2013-12-15 13:56:52 +0100

    Update error on CommunicationBus spec

--- test/specs/utils/communication-bus-spec.js
+++ test/specs/utils/communication-bus-spec.js
@@ -2,7 +2,7 @@
   define(['utils/communication-bus', 'backbone.wreqr'], function(CommunicationBus, Wreqr) {
     return describe("Communication Bus", function() {
       return it("Should have a 'vent', 'commands' and 'reqres' attributes", function() {
-        expect(CommunicationBus.vent, CommunicationBus.commands, CommunicationBus.reqres).not.toBeDefined();
+        expect(CommunicationBus.vent, CommunicationBus.commands, CommunicationBus.reqres).toBeDefined();
         expect(CommunicationBus.vent instanceof Wreqr.EventAggregator).toBeTruthy();
         expect(CommunicationBus.commands instanceof Wreqr.Commands).toBeTruthy();
         return expect(CommunicationBus.reqres instanceof Wreqr.RequestResponse).toBeTruthy();
